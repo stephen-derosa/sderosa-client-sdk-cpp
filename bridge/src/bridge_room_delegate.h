b61b114173049caf7479879f2cbeefb7fed0d045
Stephen DeRosa <stephen.derosa@livekit.io>
2026-02-12 15:41:20 -0700

    DataTracks integration through FFI
    realsense-livekit: Examples of using DataTracks, realsense and mcap
    
    bridge/tests/: integration, stress, unit

--- bridge/src/bridge_room_delegate.h
+++ bridge/src/bridge_room_delegate.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 LiveKit
+ * Copyright 2026 LiveKit
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,9 +39,14 @@ public:
   void onTrackSubscribed(livekit::Room &room,
                          const livekit::TrackSubscribedEvent &ev) override;
 
-  /// Forwards a track-unsubscribed event to LiveKitBridge::onTrackUnsubscribed().
+  /// Forwards a track-unsubscribed event to
+  /// LiveKitBridge::onTrackUnsubscribed().
   void onTrackUnsubscribed(livekit::Room &room,
                            const livekit::TrackUnsubscribedEvent &ev) override;
+
+  void onRemoteDataTrackPublished(
+      livekit::Room &room,
+      const livekit::RemoteDataTrackPublishedEvent &ev) override;
 
 private:
   LiveKitBridge &bridge_;
